d722d53caeaa2c3158709b83d75a2ff10ff5df25
nekomiya <302851306@qq.com>
2021-11-19 13:32:00 +0800

    fix photo search url

--- static/tb/track/static/js/app.js
+++ static/tb/track/static/js/app.js
@@ -203,7 +203,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "https://cdn.jsdelivr.net/gh/nekomiya-hinata/CXZX_GuyeeStaticFile@v1.1.1/static/tb/track/";
+/******/ 	__webpack_require__.p = "https://cdn.jsdelivr.net/gh/nekomiya-hinata/CXZX_GuyeeStaticFile@v1.1.2/static/tb/track/";
 /******/
 /******/ 	// on error function for async loading
 /******/ 	__webpack_require__.oe = function(err) { console.error(err); throw err; };
